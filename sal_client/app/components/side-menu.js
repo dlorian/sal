77b20a6397d3c2d299cc146a27e96e25cc7d4786
dlorian <f.dorau@ymail.com>
2015-11-15 13:30:19 +0100

    Side-Menu-Komponente gefixed.

--- sal_client/app/components/side-menu.js
+++ sal_client/app/components/side-menu.js
@@ -1,8 +1,41 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    attributeBindings:['loggedInUser'],
+
+    $sideMenu: null,
+
+    refreshSideMenuTimeout: null,
 
     didInsertElement: function() {
-        $('#side-menu').metisMenu();
+        var me = this;
+
+        this.$sideMenu = $('#side-menu')
+        this.$sideMenu.metisMenu();
+
+        this.$sideMenu.on('DOMNodeInserted', function() {
+            // Run side menu refresh only once
+            var existingTimeout = me.get('refreshSideMenuTimeout');
+            if(existingTimeout) {
+                clearTimeout(existingTimeout);
+            }
+
+            var timeout = setTimeout(me.refreshSideMenu.bind(me), 0);
+            me.set('refreshSideMenuTimeout', timeout);
+        });
+    },
+
+    isLoggedIn: function() {
+        var user = this.get('loggedInUser');
+        return (user && user.isLoggedIn);
+    }.property('loggedInUser'),
+
+    refreshSideMenu: function() {
+        console.log('To the refresh')
+        this.$sideMenu.metisMenu();
+
+        // Function of time executed
+        this.set('refreshSideMenuTimeout', null);
     }
+
 });
